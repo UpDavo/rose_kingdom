44bf270fe46003eb843b35f88f65a0c9b38a91fd
anthony.villegas <anthony.villegas@ar-holdings.com>
2024-11-22 15:10:20 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,36 @@
-//import { Portrait } from 'components/sections/portrait'
-import { PortraitAnimation, GameGlobe, NFTsection} from 'components/sections'
-import Sound from 'components/sound/sound'
+
+"use client" 
+
+import { useEffect } from "react";
+import { PortraitAnimation } from "components/sections";
+import Sound from "components/sound/sound";
 
 export default function Home() {
+  useEffect(() => {
+    const updateHeight = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+
+    // Actualiza la altura al cargar la página y al redimensionar
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+
+    // Limpia el event listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener("resize", updateHeight);
+    };
+  }, []);
+
   return (
-    <main className="h-full w-full 
-    dark:bg-secundary bg-secundary overflow-hidden">
+    <main
+      className="h-[calc(var(--vh)_*100)] w-full
+      dark:bg-secundary bg-secundary overflow-hidden"
+    >
       <Sound />
-      <PortraitAnimation/>
+      <PortraitAnimation />
     </main>
-  )
+  );
 }
